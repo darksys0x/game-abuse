832fb991f01f4079dc6b276a8abc9fb93e2b59c3
hamad-SHE <eng_cis1@hotmail.com>
2022-03-08 12:49:15 +0300

    health gun

---
+++ dllmain.cpp
@@ -0,0 +1,80 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+#include <stdio.h>
+#include <Psapi.h>
+#include <stdlib.h>
+
+//read the [player object from data section
+//140 - f0 = offsie of gun = 1
+struct Player
+{
+    char padding[0xEC];
+    DWORD health;
+    char padding2[0x50];
+    DWORD gun;
+    
+
+
+};
+
+void theGun() {
+    HANDLE Exe_baceAddress = GetModuleHandle(L"xxx.exe");
+    if (Exe_baceAddress) {
+        printf("getBaseAddress\n");
+
+        DWORD objectAddress =  *(DWORD*) ((DWORD)Exe_baceAddress + 0x187C0C);
+        Player* p = (Player*)objectAddress;
+        p->gun = 0x64;
+        printf("the gun bing %u | %d", p->gun, p->gun);
+    }
+
+}
+
+void healthAddress() {
+    HANDLE baseAddressOf_exe = GetModuleHandle(L"xxx.exe");
+    if (baseAddressOf_exe) {
+        printf("get Bases Address Of health\n");
+        DWORD objctAddress = *(DWORD*)((DWORD)baseAddressOf_exe + 0x187C0C);
+        Player* p = (Player*)objctAddress;
+        p->health = 0x64;
+        printf("health has been changed to = %u\n", p->health);
+    }
+}
+
+
+void DisplayConsole() {
+    if (AllocConsole())
+    {
+        freopen("CONIN$", "r", stdin);
+        freopen("CONOUT$", "w", stdout);
+        freopen("CONOOUT$", "w", stderr);
+    }
+}
+
+
+
+
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
+)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    {
+        DisplayConsole();
+        theGun();
+        healthAddress();
+
+        break;
+    }
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
